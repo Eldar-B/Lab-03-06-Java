5bec96f7e8dd3bcaf89ad63e36826292d71aa92c
Perlamonia <nulimr0off@gmail.com>
2024-12-22 14:44:04 +0700

    sns

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
-// Базовый класс
-abstract class Field {
+// Абстрактный базовый класс
+abstract class Field implements Cloneable {
     protected float size;
 
     public Field(float size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Field size must be greater than 0.");
+        }
         this.size = size;
     }
 
@@ -14,7 +17,18 @@ abstract class Field {
 
     public abstract float getCost();
 
-    public abstract void display();
+    @Override
+    public abstract String toString();
+
+    @Override
+    public Field clone() throws CloneNotSupportedException {
+        return (Field) super.clone(); // Мелкое клонирование
+    }
+}
+
+// Интерфейс для поля с дополнительными затратами
+interface AdditionalCost {
+    float calculateExtraCost();
 }
 
 // Класс политики ценообразования
@@ -30,46 +44,6 @@ class PricingPolicy {
         return fieldCost;
     }
 
-    public float getTimePlowing() {
-        return timePlowing;
-    }
-
-    public float getCostPlowing() {
-        return costPlowing;
-    }
-
-    public float getTimeCultivation() {
-        return timeCultivation;
-    }
-
-    public float getCostCultivation() {
-        return costCultivation;
-    }
-
-    public float getTimeRolling() {
-        return timeRolling;
-    }
-
-    public float getCostRolling() {
-        return costRolling;
-    }
-
-    public float getTimeFertilization() {
-        return timeFertilization;
-    }
-
-    public float getCostFertilization() {
-        return costFertilization;
-    }
-
-    public float getVolumeMineralFertilizers() {
-        return volumeMineralFertilizers;
-    }
-
-    public float getFinalTime() {
-        return timePlowing + timeCultivation + timeRolling + timeFertilization;
-    }
-
     public float getFinalCost() {
         return costPlowing + costCultivation + costRolling + costFertilization;
     }
@@ -77,26 +51,10 @@ class PricingPolicy {
 
 // Производный класс - Характеристики стандартного поля
 class FieldCharacteristics extends Field {
-    private float fieldCost = 0;
-    private float timePlowing = 0, costPlowing = 0;
-    private float timeCultivation = 0, costCultivation = 0;
-    private float timeRolling = 0, costRolling = 0;
-    private float costFertilization = 0, timeFertilization = 0, volumeMineralFertilizers = 0;
-    private float finalTime = 0, finalCost = 0;
+    protected float finalCost;
 
     public FieldCharacteristics(float size, PricingPolicy pp) {
         super(size);
-        this.fieldCost = pp.getFieldCost() * size;
-        this.timePlowing = pp.getTimePlowing() * size;
-        this.costPlowing = pp.getCostPlowing() * size;
-        this.timeCultivation = pp.getTimeCultivation() * size;
-        this.costCultivation = pp.getCostCultivation() * size;
-        this.timeRolling = pp.getTimeRolling() * size;
-        this.costRolling = pp.getCostRolling() * size;
-        this.costFertilization = pp.getCostFertilization() * size;
-        this.timeFertilization = pp.getTimeFertilization() * size;
-        this.volumeMineralFertilizers = pp.getVolumeMineralFertilizers() * size;
-        this.finalTime = pp.getFinalTime() * size;
         this.finalCost = pp.getFinalCost() * size;
     }
 
@@ -106,15 +64,39 @@ class FieldCharacteristics extends Field {
     }
 
     @Override
-    public void display() {
-        System.out.println("Field size: " + size + " He");
-        System.out.println("Field cost: " + fieldCost + " rub");
-        System.out.println("Final cost: " + finalCost + " rub");
+    public String toString() {
+        return "FieldCharacteristics: size=" + size + " He, cost=" + finalCost + " rub";
     }
 }
 
-// Производный класс - Особенное поле
-class SpecialField extends Field {
+// Производный класс - Поле с орошением
+class IrrigatedField extends FieldCharacteristics implements AdditionalCost {
+    private float irrigationCost;
+
+    public IrrigatedField(float size, PricingPolicy pp, float irrigationCost) {
+        super(size, pp); // Вызов конструктора базового класса
+        this.irrigationCost = irrigationCost;
+    }
+
+    @Override
+    public float calculateExtraCost() {
+        return irrigationCost * size;
+    }
+
+    @Override
+    public float getCost() {
+        // Перегрузка метода с вызовом метода базового класса
+        return super.getCost() + calculateExtraCost();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", irrigationCost=" + calculateExtraCost() + " rub";
+    }
+}
+
+// Класс, реализующий глубокое клонирование
+class SpecialField extends Field implements Cloneable {
     private float extraCost;
 
     public SpecialField(float size, float extraCost) {
@@ -128,9 +110,31 @@ class SpecialField extends Field {
     }
 
     @Override
-    public void display() {
-        System.out.println("Special field size: " + size + " He");
-        System.out.println("Special field cost: " + getCost() + " rub");
+    public String toString() {
+        return "SpecialField: size=" + size + " He, cost=" + getCost() + " rub";
+    }
+
+    @Override
+    public SpecialField clone() {
+        return new SpecialField(size, extraCost); // Глубокое клонирование
+    }
+}
+
+// Шаблонный класс
+class Box<T extends Field> {
+    private T field;
+
+    public Box(T field) {
+        this.field = field;
+    }
+
+    public T getField() {
+        return field;
+    }
+
+    @Override
+    public String toString() {
+        return "Box contains: " + field.toString();
     }
 }
 
@@ -145,11 +149,12 @@ public class Main {
         do {
             System.out.println("================================");
             System.out.println("1. Add standard field");
-            System.out.println("2. Add special field");
-            System.out.println("3. Show all fields");
-            System.out.println("4. Sort fields by cost");
-            System.out.println("5. Search field by size");
-            System.out.println("6. Exit");
+            System.out.println("2. Add irrigated field");
+            System.out.println("3. Add special field");
+            System.out.println("4. Show all fields");
+            System.out.println("5. Clone a field");
+            System.out.println("6. Sort fields by cost");
+            System.out.println("7. Exit");
             System.out.println("================================");
             choice = scanner.nextInt();
 
@@ -159,37 +164,40 @@ public class Main {
                     float size = scanner.nextFloat();
                     fields.add(new FieldCharacteristics(size, pp));
                 } else if (choice == 2) {
+                    System.out.print("Enter field size in He: ");
+                    float size = scanner.nextFloat();
+                    System.out.print("Enter irrigation cost per hectare: ");
+                    float irrigationCost = scanner.nextFloat();
+                    fields.add(new IrrigatedField(size, pp, irrigationCost));
+                } else if (choice == 3) {
                     System.out.print("Enter special field size in He: ");
                     float size = scanner.nextFloat();
                     System.out.print("Enter extra cost per hectare: ");
                     float extraCost = scanner.nextFloat();
                     fields.add(new SpecialField(size, extraCost));
-                } else if (choice == 3) {
-                    for (Field field : fields) {
-                        field.display();
-                    }
                 } else if (choice == 4) {
+                    for (Field field : fields) {
+                        System.out.println(field);
+                    }
+                } else if (choice == 5) {
+                    System.out.print("Enter index of the field to clone: ");
+                    int index = scanner.nextInt();
+                    if (index >= 0 && index < fields.size()) {
+                        Field clonedField = fields.get(index).clone();
+                        fields.add(clonedField);
+                        System.out.println("Field cloned successfully.");
+                    } else {
+                        System.out.println("Invalid index.");
+                    }
+                } else if (choice == 6) {
                     fields.sort(Comparator.comparing(Field::getCost));
                     System.out.println("Fields sorted by cost.");
-                } else if (choice == 5) {
-                    System.out.print("Enter field size to search: ");
-                    float size = scanner.nextFloat();
-                    Field result = fields.stream()
-                            .filter(field -> field.getSize() == size)
-                            .findFirst()
-                            .orElse(null);
-                    if (result != null) {
-                        System.out.println("Field found:");
-                        result.display();
-                    } else {
-                        System.out.println("No field found with size " + size + " He.");
-                    }
                 }
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
                 scanner.nextLine();
             }
-        } while (choice != 6);
+        } while (choice != 7);
 
         scanner.close();
     }
